5cf035f1786d7e1ba3989dc49c1e5c02f7103dc0
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2024-12-25 16:46:56 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -9,47 +9,20 @@ class Game {
 
 // Load the background 
     init() {
-        const image = new Image();
-        image.onload = () => {
-            //for the background stage to fit the canvas entirely
-          const aspectRatio = image.width / image.height; //Why is the aspect ratio needed?
-          // Set the canvas width and height
-          const canvasWidth = this.canvas.width;
-          const canvasHeight = this.canvas.height;
-          let newWidth = canvasWidth;
-          let newHeight = canvasWidth / aspectRatio;
-          if (newHeight > canvasHeight) {
-            newHeight = canvasHeight;
-            newWidth = canvasHeight * aspectRatio;
-          }
-          this.ctx.drawImage(image, 0, 0, newWidth, newHeight);
-        };
-        image.src = "/assets/batcave.png";
         
-      //
-            //hero
+      const cave = new Stage("../assets/batcave.png",this.canvas.width,this.canvas.height,this.ctx);
+        
+
       const centerX=this.canvas.width / 2;
       const centerY= this.canvas.height / 2;
-      //
+      //hero ========================================================================================================================
    
-      const hero = new Image();
-      hero.onload = () => {
-        const scaleFactor=0.03;
-        this.ctx.drawImage(
-          hero,
-          0, //left cut
-          0, //top cut,
-          hero.width, //width of cut
-          hero.height, //height of cut
-          centerX-60, //x position at center
-          centerY,//y position at center
-          hero.width*scaleFactor,
-          hero.height*scaleFactor
-       )
-      }
-      //player
-      hero.src = "/assets/tiny_barry.png";
-    
+      const hero = new Sprite("../assets/tiny_barry.png",
+        centerX-60,centerY,0.05,this.ctx
+      );
+      hero.draw();
+      hero.move(5);
+      //hero ========================================================================================================================
     }
    
    }
